a39074dbcd702c8992c909eea80d0e6b3b454fcb
Vignesh Parmar <vigneshparmar562@gmail.com>
2026-01-21 22:29:04 +0530

    Order controller add

---
+++ Neighborhood/app/Http/Controllers/OrderController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::where('user_id', auth()->id())
+            ->with('orderItems.product')
+            ->latest()
+            ->get();
+        
+        return view('orders.index', compact('orders'));
+    }
+
+    public function show(Order $order)
+    {
+        if ($order->user_id !== auth()->id()) {
+            abort(403);
+        }
+        
+        $order->load('orderItems.product.seller');
+        
+        return view('orders.show', compact('order'));
+    }
+}
